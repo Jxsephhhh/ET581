e3d2326d214a9a737aab3999d37c76c7dca4434d
Jxsephhhh <joseph.bernabe89@login.cuny.edu>
2025-08-27 00:30:41 +0000

    Save Hello World

---
+++ Lab1/Ex1_HelloWorld.java
@@ -0,0 +1,9 @@
+package Lab1;
+
+public class Ex1_HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("this is not new line");
+        System.out.println("Hello World!");
+        System.out.print("this is not new line");
+    }
+}
