e3e09a143cab1e9094bc5c3ee55bd6606d878526
Joseph Bernabe <josephbernabe89@student.qcc.cuny.edu>
2025-09-02 18:58:39 -0400

    updated

---
+++ Lab1/Exercise_8.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Exercise8 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Step 1: Prompt user
+        System.out.print("Enter a 4-digit integer: ");
+        int number = input.nextInt();
+
+        // Step 2: Extract digits using division and modulus
+        int firstDigit = number / 1000;              // thousands place
+        int secondDigit = (number / 100) % 10;       // hundreds place
+        int thirdDigit = (number / 10) % 10;         // tens place
+        int fourthDigit = number % 10;               // ones place
+
+        // Step 3: Print results
+        System.out.println("first digit: " + firstDigit);
+        System.out.println("second digit: " + secondDigit);
+        System.out.println("third digit: " + thirdDigit);
+        System.out.println("fourth digit: " + fourthDigit);
+    }
+}
